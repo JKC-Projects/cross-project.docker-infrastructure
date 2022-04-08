f3e4ab24932a53fe1b98f7018837fb08fd6a76d4
JK-Chung <chungjeeken@outlook.com>
2022-04-08 18:13:57 +0100

    made arch match between instance type and ami

--- input_variables.tf
+++ input_variables.tf
@@ -4,6 +4,6 @@ variable "instance_type_for_ec2_in_ecs_cluster" {
 }
 
 variable "ami_id_for_ec2_in_ecs_cluster" {
-  default = "ami-0069d66985b09d219" # Amazon Linux 2 (Kernel 5.10)
+  default = "ami-02e875830ca47abb5" # Amazon Linux 2 (Kernel 5.10) (arm64)
   type    = string
 }
