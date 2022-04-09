50dcb3a683aba5b72d80bd1107d6f61463dc255e
JK-Chung <chungjeeken@outlook.com>
2022-04-09 21:49:03 +0100

    making arch of ami and instance type match up

--- input_variables.tf
+++ input_variables.tf
@@ -4,6 +4,6 @@ variable "instance_type_for_ec2_in_ecs_cluster" {
 }
 
 variable "ami_id_for_ec2_in_ecs_cluster" {
-  default = "ami-0bb7c2ee184bc8249" # /aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id
+  default = "ami-09ed245c9581057bd" # /aws/service/ecs/optimized-ami/amazon-linux-2/arm64/recommended/image_id
   type    = string
 }
