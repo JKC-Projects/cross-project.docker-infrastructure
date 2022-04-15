af22982a7d80531cfaadb9cc8da6e50ceca69da4
JK-Chung <chungjeeken@outlook.com>
2022-04-15 16:34:14 +0100

    added ssm params required to deploy containers into the ECS cluster

--- output_ssm.tf
+++ output_ssm.tf
@@ -1,6 +1,13 @@
-resource "aws_ssm_parameter" "public_subnet_ids" {
+resource "aws_ssm_parameter" "ecs_deployment_artifacts_url" {
   name        = "/ecr/deployment-artifacts"
   type        = "String"
   value       = aws_ecr_repository.artifacts_ecr.repository_url
   description = "The URL of the ECR Deployment Artifacts repository where you can push/pull Docker images"
+}
+
+resource "aws_ssm_parameter" "ecs_ec2_cluster" {
+  name        = "/ecs/ec2-cluster"
+  type        = "String"
+  value       = aws_ecs_cluster.ec2_cluster.name
+  description = "The name of the ECS cluster (${aws_ecs_cluster.ec2_cluster.name}) used to deploy Docker containers"
 }
