3482625e3295c824fe51a220115c0d903ddbb771
JK-Chung <chungjeeken@outlook.com>
2022-05-04 20:12:56 +0100

    Small refactor

--- locals.tf
+++ locals.tf
@@ -6,5 +6,12 @@ locals {
     }
   ]
 
-  ecr_artifacts_repos = [for a in local.application : "${a.project}.${a.application}"]
+  # the key to this will be used to name the corresponding deployment-artifacts repo
+  ecr_artifacts_repos = [for a in local.applications :
+    {
+      ecr_repo_name = "${a.project}.${a.application}"
+      project       = a.project
+      application   = a.application
+    }
+  ]
 }
