90c084f2b41cdb241a70b0710d624b15eb5aafee
JK-Chung <chungjeeken@outlook.com>
2022-05-04 19:59:03 +0100

    Corrected

--- output_ssm.tf
+++ output_ssm.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "ecs_deployment_artifacts_url" {
   for_each    = { for a in local.ecr_artifacts_repos : a.ecr_repo_name => a }
   name        = format("/ecr/deployment-artifacts/%s/%s", each.value.project, each.value.application)
   type        = "String"
-  value       = aws_ecr_repository.artifacts_ecr[each.key.repository_url].repository_url
+  value       = aws_ecr_repository.artifacts_ecr[each.key].repository_url
   description = format("[%s] The URL of the ECR Deployment Artifacts repository where you can push/pull Docker images", each.key)
 }
 
